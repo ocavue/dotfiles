450f800484a41dedcc41d64e137aed930281f3f6
ocavue <ocavue@gmail.com>
2021-05-07 18:23:52 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ set -ex
 # debug log
 echo "Running setup.sh $(whoami) $(date)" > $HOME/.dotfiles.setup.log
 
+# get absolute path of dotfiles
+cd $(dirname $0)
+DOTFILES_PATH=$PWD
+
 # switch the default shell to `zsh`
 sudo chsh -s /bin/zsh
 
@@ -14,21 +18,30 @@ then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+function git_clone {
+    $remote=$0
+    $local=$1
+    if [ ! -d "$local" ]
+    then
+        git clone --depth=1 $remote $local
+    fi
+}
+
 # install z
-git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-z
+git_clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-z
 
 # install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git_clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+git_clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
 
 # install iTerm2 Shell Integration
 # document: https://iterm2.com/documentation-shell-integration.html
 # curl -L https://iterm2.com/shell_integration/zsh -o $HOME/.iterm2_shell_integration.zsh
 
 # setup zsh config
-ln -sf $(dirname $0)/zsh/.zlogin $HOME/.zlogin
+ln -sf $DOTFILES_PATH/zsh/.zlogin $HOME/.zlogin
 
 # setup theme
-ln -sf $(dirname $0)/zsh/.p10k.zsh $HOME/.p10k.zsh
+ln -sf $DOTFILES_PATH/zsh/.p10k.zsh $HOME/.p10k.zsh
